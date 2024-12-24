ce1df2459033d2acb54b23c2642c0f9b9684e802
Tom <tommapcoder@gmail.com>
2024-12-24 13:26:55 +0700

    default constructor

--- src/main/java/org/tommap/springmapstruct/target_package/PersonDTO.java
+++ src/main/java/org/tommap/springmapstruct/target_package/PersonDTO.java
@@ -1,11 +1,15 @@
 package org.tommap.springmapstruct.target_package;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonDTO {
     String firstName;
     String lastName;
